e938396b7759826d905d444a02c935676e0e9fdf
laurigust <101134587+laurigust@users.noreply.github.com>
2022-05-19 19:54:51 +0300

    Pakeitimai

--- src/components/About.js
+++ src/components/About.js
@@ -17,6 +17,48 @@ function About() {
         <div className="column is-one-third">
         <div className="field">
         <label className="label" htmlFor="text">MirGOsof teams project for: "Programavimo indžinerija".</label>
+<<<<<<< Updated upstream
+=======
+        <label className="text" htmlFor="text">MirGOsof teams - tai 2022 metais KTU multimedijos studentų įkurta įmonė. 
+        MirGOstore internetinė parduotuvė užsiima įvairiausių prekių pardavimu. Parduotuvėje galima įsigyti elektronikos, namų buities prekes, sodo technikos ir kitų
+        produktų. Nors parduotuvė įkurta tik 2022 metais, tačiau tai jau yra didžiausia elektroninė parduotuvė Baltijos šalyse ir yra laimėjusi Lietuvos
+        parduotuvių apdovanojimą "Metų parduotuvė 2022".
+        <br>
+        </br>
+        Klientų nuomone MirGOstore yra viena patikimiausių ir greičiausiai prekes pristančių parduotuvių Lietuvoje.
+         </label>
+        <br>
+        </br>
+        <br>
+        </br>
+        <label className="label" htmlFor="text">Kontaktai.</label>
+        <label className="text" htmlFor="text">El. paštas: mirgostore@gmail.com.</label>
+        <br>
+        </br>
+        <label className="text" htmlFor="text">tel. nr.: 867700012.</label>
+        <br>
+        </br>
+        <br>
+        </br>
+        <label className="label" htmlFor="text">Projekto komanda:</label>
+        <label className="text" htmlFor="text">Justina Krikščikaitė</label>
+        <br>
+        </br>
+        <label className="text" htmlFor="text">Žilvinas Žitkauskas</label>
+        <br>
+        </br>
+        <label className="text" htmlFor="text">Žygimantas Pipiras</label>
+        <br>
+        </br>
+        <label className="text" htmlFor="text">Laurynas Gustas</label>
+        <br>
+        </br>
+        <br>
+        </br>
+        <br>
+        </br>
+        <label className="text" htmlFor="text">Ši parduotuvė - tai Programavimo indžinerijos modulio projektas.</label>
+>>>>>>> Stashed changes
         </div>
         </div>
       </div>
